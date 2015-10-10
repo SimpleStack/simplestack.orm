2f4450c1ae70e53c0840a97e8c32c5d473717a96
Vincent DARON <vda@depfac.com>
2015-10-10 20:07:26 +0200

    Add SolutionInfo.cs

--- SimpleStack.Orm/Properties/AssemblyInfo.cs
+++ SimpleStack.Orm/Properties/AssemblyInfo.cs
@@ -7,30 +7,4 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleStack.Orm")]
 [assembly: AssemblyDescription("Lite ORM framework based on Dapper and Query Generation from ServiceStack/NServicekit")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Vincent DARON & Contributors")]
-[assembly: AssemblyProduct("SimpleStack.Orm")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e88b9b0a-d784-4b2f-8abe-02fea2192b18")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SolutionInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Vincent DARON & Contributors")]
+[assembly: AssemblyProduct("SimpleStack.Orm")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
