5818a8a13d85e82e8c68829ba2ec7ce5bcdecb10
Vincent DARON <vda@depfac.com>
2015-10-10 19:56:24 +0200

    Add asynchronous test

---
+++ SimpleStack.Orm.Tests/AsynchronousTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SimpleStack.Orm.Tests
+{
+	public partial class ExpressionTests
+	{
+		[Test]
+		public void Can_select_async()
+		{
+			var expected = new TestType()
+			{
+				IntColumn = 7,
+				BoolColumn = true,
+				StringColumn = "test"
+			};
+
+			EstablishContext(10, expected);
+
+			var actual = OpenDbConnection().SelectAsync<TestType>(q => q.IntColumn == 7).Result;
+
+			Assert.IsNotNull(actual);
+			Assert.AreEqual(1, actual.Count());
+			CollectionAssert.Contains(actual, expected);
+		}
+	}
+}
