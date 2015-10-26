4e2353a95ff37dad75cb183a9ed306e38b46158f
Vincent DARON <vda@depfac.com>
2015-10-26 21:22:25 +0100

    Fix nuget references and add AssemblyInformationalVersion attribute

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("Vincent DARON & Contributors")]
+[assembly: AssemblyCompany("Vincent DARON &amp; Contributors")]
 [assembly: AssemblyProduct("SimpleStack.Orm")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-rc2")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
