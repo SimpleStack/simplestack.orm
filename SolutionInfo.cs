c278c59812466ebfb2ea04843306d02d31be8427
Vincent DARON <vda@depfac.com>
2016-05-22 21:14:37 +0200

    Release 1.0-rc8

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-rc7")]
+[assembly: AssemblyInformationalVersion("1.0.0-rc8")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
