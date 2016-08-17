62e70e838cec30107b3bc42e904979320bf91331
Vincent DARON <vda@depfac.com>
2016-08-17 22:40:05 +0200

    Add pack command batch script

---
+++ pack.bat
@@ -0,0 +1,6 @@
+dotnet pack -c Release -o nuget src\simplestack.orm
+dotnet pack -c Release -o nuget src\simplestack.orm.MySQL
+dotnet pack -c Release -o nuget src\simplestack.orm.MySQLConnector
+dotnet pack -c Release -o nuget src\simplestack.orm.PostgreSQL
+dotnet pack -c Release -o nuget src\simplestack.orm.SqLite
+dotnet pack -c Release -o nuget src\simplestack.orm.SqlServer
