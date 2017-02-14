f1da5fd7ca7ba3936ee4474805ef9367dd147928
Vincent DARON <vda@depfac.com>
2017-02-14 13:14:13 +0100

    Add TransactionScope

---
+++ src/SimpleStack.Orm/TransactionScope.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+#if !NETSTANDARD1_5
+using System.Runtime.Remoting.Messaging;
+#endif
+
+namespace SimpleStack.Orm
+{
+
+	public class TransactionScope : IDisposable
+	{
+#if NETSTANDARD1_5
+		private readonly AsyncLocal<TransactionScope> _rootConnectionScope = new AsyncLocal<TransactionScope>();
+#endif
+		private OrmConnection _rootScopeConnection;
+		private IDbTransaction _transaction;
+
+		public TransactionScope(OrmConnectionFactory connectionFactory)
+		{
+			if (RootScope == null)
+			{
+				// Create DB Connection scope
+				_rootScopeConnection = connectionFactory.OpenConnection();
+
+				// Create a transaction
+				_transaction = _rootScopeConnection.BeginTransaction();
+
+				// Setup self as the root scope
+				RootScope = this;
+			}
+
+			Completed = false;
+		}
+
+		public TransactionScope RootScope
+		{
+#if NETSTANDARD1_5
+			get { return _rootConnectionScope.Value; }
+			set { _rootConnectionScope.Value = value; }
+#else
+			get { return (TransactionScope)CallContext.LogicalGetData("_rootScope"); }
+			set { CallContext.LogicalSetData("_rootScope",value); }
+#endif
+		}
+
+		public bool Completed { get; private set; }
+
+		public OrmConnection Connection => RootScope.Connection;
+
+		public void Complete()
+		{
+			// Set completed flag
+			Completed = true;
+
+			// Don't complete if we're not the root scope.
+			if (RootScope != this)
+			{
+				return;
+			}
+
+			//Logger.DebugFormat("Committing Transaction");
+			_transaction?.Commit();
+
+			Cleanup();
+		}
+
+		public void Cancel()
+		{
+			// If we are not the root scope, cancel the entire transaction
+			if (RootScope != null && RootScope != this)
+			{
+				RootScope.Cancel();
+				return;
+			}
+
+			_transaction?.Rollback();
+
+			Cleanup();
+		}
+
+		public void Dispose()
+		{
+			if (!Completed)
+			{
+				Cancel();
+			}
+		}
+
+		private void Cleanup()
+		{
+			if (_transaction != null)
+			{
+				_transaction.Dispose();
+				_transaction = null;
+			}
+
+			if (_rootScopeConnection != null)
+			{
+				_rootScopeConnection.Dispose();
+				_rootScopeConnection = null;
+			}
+
+			RootScope = null;
+		}
+	}
+
+}
---
+++ test/SimpleStack.Orm.Tests/ConnectionScopeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SimpleStack.Orm.Tests
+{
+
+	public partial class ExpressionTests 
+	{
+		[Test]
+		public void Test_ConnectionScope_Throw_Exception()
+		{
+			var connFac = new OrmConnectionFactory(_dialectProvider, ConnectionString);
+
+			Assert.Throws(typeof(Exception), () =>
+			{
+				using (var scope1 = new TransactionScope(connFac))
+				{
+					using (var scope2 = new TransactionScope(connFac))
+					{
+						using (var scope3 = new TransactionScope(connFac))
+						{
+							throw new Exception();
+							scope3.Complete();
+						}
+						scope2.Complete();
+					}
+					scope1.Complete();
+				}
+			});
+		}
+
+
+		[Test]
+		public void ConnectionScope_1()
+		{
+			var connFac = new OrmConnectionFactory(_dialectProvider, ConnectionString);
+
+			using (var scope = new TransactionScope(connFac))
+			{
+				scope.Connection.Select<TestType>();
+
+				scope.Complete();
+
+				Assert.IsTrue(scope.Completed);
+			}
+		}
+	}
+
+}
