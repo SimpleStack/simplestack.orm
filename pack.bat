a914684545c4d1f958a2e6aaac99f39a89a191cd
Vincent DARON <vda@dfakto.com>
2017-08-24 05:41:52 -0700

    Fix Nuget pack script

--- pack.bat
+++ pack.bat
@@ -1,12 +1,12 @@
 dotnet restore src/SimpleStack.Orm
-dotnet pack -c Release -o nuget src/SimpleStack.Orm
+dotnet pack -c Release -o ../../nuget src/SimpleStack.Orm
 dotnet restore src/SimpleStack.Orm.MySQL
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.MySQL
+dotnet pack -c Release -o ../../nuget src/SimpleStack.Orm.MySQL
 dotnet restore src/SimpleStack.Orm.MySQLConnector
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.MySQLConnector
+dotnet pack -c Release -o ../../nuget src/SimpleStack.Orm.MySQLConnector
 dotnet restore src/SimpleStack.Orm.PostgreSQL
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.PostgreSQL
+dotnet pack -c Release -o ../../nuget src/SimpleStack.Orm.PostgreSQL
 dotnet restore src/SimpleStack.Orm.SQLite
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.SQLite
+dotnet pack -c Release -o ../../nuget src/SimpleStack.Orm.SQLite
 dotnet restore src/SimpleStack.Orm.SQLServer
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.SQLServer
+dotnet pack -c Release -o ../../nuget src/SimpleStack.Orm.SQLServer
