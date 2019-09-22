6c79aa6b9156d7e032871fbf6fd2c7bf7f2e7ace
Vincent DARON <vda@dfakto.com>
2019-09-22 09:02:49 +0200

    Fix pack.sh

--- pack.sh
+++ pack.sh
@@ -9,6 +9,6 @@ dotnet pack -c Release -o nuget src/SimpleStack.Orm.PostgreSQL
 dotnet restore src/SimpleStack.Orm.SQLite
 dotnet pack -c Release -o nuget src/SimpleStack.Orm.SQLite
 dotnet restore src/SimpleStack.Orm.SDSQlite
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.SDSQLite
+dotnet pack -c Release -o nuget src/SimpleStack.Orm.SDSQlite
 dotnet restore src/SimpleStack.Orm.SQLServer
 dotnet pack -c Release -o nuget src/SimpleStack.Orm.SQLServer
