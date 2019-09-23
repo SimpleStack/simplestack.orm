758a96826c3d4ac4addf8dd3d730ca9dcab801bc
Vincent DARON <vda@dfakto.com>
2019-09-23 07:41:19 +0200

    Release 1.2.0-beta9

--- pack.sh
+++ pack.sh
@@ -1,14 +1,14 @@
 dotnet restore src/SimpleStack.Orm
-dotnet pack -c Release -o nuget src/SimpleStack.Orm
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm
 dotnet restore src/SimpleStack.Orm.MySQL
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.MySQL
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm.MySQL
 dotnet restore src/SimpleStack.Orm.MySQLConnector
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.MySQLConnector
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm.MySQLConnector
 dotnet restore src/SimpleStack.Orm.PostgreSQL
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.PostgreSQL
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm.PostgreSQL
 dotnet restore src/SimpleStack.Orm.SQLite
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.SQLite
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm.SQLite
 dotnet restore src/SimpleStack.Orm.SDSQlite
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.SDSQlite
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm.SDSQlite
 dotnet restore src/SimpleStack.Orm.SQLServer
-dotnet pack -c Release -o nuget src/SimpleStack.Orm.SQLServer
+dotnet pack -c Release -o ../nuget src/SimpleStack.Orm.SQLServer
