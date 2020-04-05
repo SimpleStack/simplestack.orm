4568d8da4f0aa91044dc4eafc3a67f863afea2d5
Vincent DARON <vda@dfakto.com>
2020-04-05 09:08:12 +0200

    Add group by test

--- test/SimpleStack.Orm.Tests/SelectCountTests.cs
+++ test/SimpleStack.Orm.Tests/SelectCountTests.cs
@@ -58,5 +58,34 @@ namespace SimpleStack.Orm.Tests
 				})); // SELECT COUNT (DISTINCT Age) FROM Person
 			}
 		}
+		
+		[Test]
+		public void Can_select_count_with_group_by()
+		{
+			OpenDbConnection().Insert(new Person { Id = 1, Age = 5 });
+			OpenDbConnection().Insert(new Person { Id = 2, Age = 10 });
+			OpenDbConnection().Insert(new Person { Id = 3, Age = 10 });
+			OpenDbConnection().Insert(new Person { Id = 4, Age = 20 });
+			OpenDbConnection().Insert(new Person { Id = 5, Age = 20 });
+			OpenDbConnection().Insert(new Person { Id = 6, Age = 25 });
+
+			using (var conn = OpenDbConnection())
+			{
+				var rr = conn.Select<Person>(x =>
+				{
+					//x.Select(y => new {Age = y.Age, Id = Sql.Max(y.Id)});
+					x.Select(y => new Person{Age = y.Age, Id = Sql.Max(y.Id)});
+					//x.SelectAlso(y => Sql.Max(y.Id));
+					x.GroupBy(y => y.Age);
+				});
+				
+				
+				Assert.AreEqual(4, conn.Count<Person>(x =>
+				{
+					x.Distinct();
+					x.Select(y => y.Age);
+				})); // SELECT COUNT (DISTINCT Age) FROM Person
+			}
+		}
 	}
 }
