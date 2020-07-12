4d299ce0793e5c5dd5c8fa857994ca748053bc4b
Vincent DARON <vda@dfakto.com>
2020-07-12 22:32:19 +0200

    remove unused using

--- src/SimpleStack.Orm.SQLServer/SqlServerOrmLiteDialectProvider.cs
+++ src/SimpleStack.Orm.SQLServer/SqlServerOrmLiteDialectProvider.cs
@@ -2,14 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using Dapper;
 using Microsoft.Data.SqlClient;
-using SimpleStack.Orm.Attributes;
-using SimpleStack.Orm.Expressions;
 using SimpleStack.Orm.Expressions.Statements;
 
 namespace SimpleStack.Orm.SqlServer
