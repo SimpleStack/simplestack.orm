e8dbf374c60805e38a0d52060650fbcecc4f8f48
avtc <tarasenkov@gmail.com>
2022-02-07 20:14:30 +0200

    Workaround for PostgreSQL lastval() when table does not have identity column return 0. Require setting AutoIncrementAttribute on Identity columns.

--- src/SimpleStack.Orm/Expressions/Statements/InsertStatement.cs
+++ src/SimpleStack.Orm/Expressions/Statements/InsertStatement.cs
@@ -5,5 +5,6 @@ namespace SimpleStack.Orm.Expressions.Statements
     public class InsertStatement : Statement
     {
         public List<string> InsertFields { get; } = new List<string>();
+        public bool HasIdentity { get; set; }
     }
 }
